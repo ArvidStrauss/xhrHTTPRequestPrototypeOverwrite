725d6128985c7cdeec3687c93d1eab48037764e1
Marcus Biel <marcus.biel@t-systems.com>
2020-07-08 15:33:35 +0200

    fix document view tracking, scoping

--- common/click_tracking.js
+++ common/click_tracking.js
@@ -5,19 +5,20 @@
 /* Updated: _$DATE$_
 /** ######################## **/
 
-var ignore = 'data-piwikignore';
-var ignoreSelector = ':not(['+ignore+']';
-
-if(TRACKINGSETTINGS.TRACKEXTERNALFILES) {
-    var FILELINKSELECTOR = '';
-    for(var i=0; i<coyoTrackingUtils.FILEEXTENSIONS.length;i++){
-        FILELINKSELECTOR += 'a[href$=".' + coyoTrackingUtils.FILEEXTENSIONS[i] + '"]'+':not(['+ignore+'-extfile])';
-        if(i !== coyoTrackingUtils.FILEEXTENSIONS.length-1) FILELINKSELECTOR += ', ';
-    }
-}
-
 //click event tracking
 var coyoClickTracking = {
+    init: function() {
+        coyoClickTracking.ignore = 'data-piwikignore';
+        coyoClickTracking.ignoreSelector = ':not(['+coyoClickTracking.ignore+']';
+        coyoUrl = window.location.protocol + '//' + window.location.hostname;
+        if(TRACKINGSETTINGS.TRACKEXTERNALFILES) {
+            coyoClickTracking.FILELINKSELECTOR = 'a[target="_blank"]:not(['+coyoClickTracking.ignore+'-extfile]),';
+            for(var i=0; i<coyoTrackingUtils.FILEEXTENSIONS.length;i++){
+                coyoClickTracking.FILELINKSELECTOR += 'a[href$=".' + coyoTrackingUtils.FILEEXTENSIONS[i] + '"]'+':not(['+coyoClickTracking.ignore+'-extfile]):not([href^="/"]):not([href^="'+coyoUrl+'"])';
+                if(i !== coyoTrackingUtils.FILEEXTENSIONS.length-1) coyoClickTracking.FILELINKSELECTOR += ', ';
+            }
+        }
+    },
     filterClickTracking: function(category, item) {
         item.addEventListener('click', function() {
             var name = "";
@@ -34,12 +35,12 @@ var coyoClickTracking = {
     // the attribute data-title is set by listening to the stream HEAD request in tracking.js
     // if there is no such attribute, this failed because it was not possible to parse a title or match the response with an element
     addVideoTracking: function() {
-        document.querySelectorAll('video'+ignoreSelector).forEach(function(item) {
+        document.querySelectorAll('video'+coyoClickTracking.ignoreSelector).forEach(function(item) {
             console.debug('found video:', item);
             // if a default uploaded video is embedded in a post, there is no information about the title
             // look into objectDB, otherwise send a HEAD request to the source, parse the title from the header, add the data-title attribute to that video
             // then track it with its title on first play
-            item.setAttribute(ignore, "true");
+            item.setAttribute(coyoClickTracking.ignore, "true");
             if(!item.getAttribute('data-title')) {
                 console.debug('no title found, try to get it...');
                 coyoTrackingUtils.getVideoInfo(item.src,function(filename){
@@ -60,14 +61,14 @@ var coyoClickTracking = {
                     },500);
                 }
             });
-            item.setAttribute(ignore, "true");
+            item.setAttribute(coyoClickTracking.ignore, "true");
         });
     },
 
     addClickBindings: function(){
         // Header Navigation
         if(TRACKINGSETTINGS.NAV_MAIN) {
-            document.querySelectorAll(SELECTORS.HEAD_NAVIGATION+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.HEAD_NAVIGATION+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 // skip mobile menu button
                 if(item.getAttribute('ng-click') === '$ctrl.openMenu()') return true
                 item.addEventListener('click', function(e){
@@ -75,72 +76,72 @@ var coyoClickTracking = {
                     name = item.className === 'nav-item-brand' ? 'Home' :item.querySelector('.nav-item-label').textContent.trim();
                     sendTrackingEvent('Header', coyoTrackingUtils.typeNameOverrides('Click'), coyoTrackingUtils.typeNameOverrides(name), null);
                 });
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
         }
         // Header Subnavigation
         if(TRACKINGSETTINGS.NAV_SUB) {
-            document.querySelectorAll(SELECTORS.HEAD_SUBNAVIGATION+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.HEAD_SUBNAVIGATION+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 item.addEventListener('click', function(e){
                     var name='Navigation';
                     name = item.textContent.trim();
                     sendTrackingEvent('Navi-Top', coyoTrackingUtils.typeNameOverrides('Click'), coyoTrackingUtils.typeNameOverrides(name), null);
                 });
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
         }
         // Mobile Sidebar Navigation
         if(TRACKINGSETTINGS.NAV_MOBILE) {
-            document.querySelectorAll(SELECTORS.NAV_SIDEBAR+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.NAV_SIDEBAR+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 item.addEventListener('click', function(e){
                     if(coyoTrackingUtils.excludeUrl(window.location.href)) return;
                     var name='Navigation';
                     name = item.textContent.trim();
                     sendTrackingEvent('Navi-Mobile', coyoTrackingUtils.typeNameOverrides('Click'), coyoTrackingUtils.typeNameOverrides(name), null);
                 });
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
         }
         if(TRACKINGSETTINGS.SIDEBAR_SUBSCRIPTIONS) {
-            document.querySelectorAll(SELECTORS.SIDEBAR_SUBSCRIPTIONS+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.SIDEBAR_SUBSCRIPTIONS+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 item.addEventListener('click', function(){
                     var type = /\/pages\//gi.exec(item.href) ? 'widget-my-pages' : 'widget-my-workspaces';
                     var name = item.querySelector('span').textContent.trim();
                     sendTrackingEvent(type, coyoTrackingUtils.typeNameOverrides('Click'), coyoTrackingUtils.typeNameOverrides(name), null);
                 });
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
         }
         if(TRACKINGSETTINGS.SIDEBAR_BOOKMARKS) {
-            document.querySelectorAll(SELECTORS.SIDEBAR_BOOKMARKS+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.SIDEBAR_BOOKMARKS+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 item.addEventListener('click', function(e){
                     var name = item.textContent.trim();
                     sendTrackingEvent('widget-bookmarks', coyoTrackingUtils.typeNameOverrides('Click'), coyoTrackingUtils.typeNameOverrides(name), null);
                 });
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
         }
         if(TRACKINGSETTINGS.SEARCHFILTER) {
-            document.querySelectorAll(SELECTORS.SEARCHFILTER_TYPE+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.SEARCHFILTER_TYPE+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 coyoClickTracking.filterClickTracking(coyoTrackingUtils.typeNameOverrides('Search-Type'),item);
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
-            document.querySelectorAll(SELECTORS.SEARCHFILTER_MODIFIED+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.SEARCHFILTER_MODIFIED+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 coyoClickTracking.filterClickTracking(coyoTrackingUtils.typeNameOverrides('Search-Modified'),item);
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
-            document.querySelectorAll(SELECTORS.SEARCHFILTER_LOCATION+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.SEARCHFILTER_LOCATION+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 coyoClickTracking.filterClickTracking(coyoTrackingUtils.typeNameOverrides('Search-Location'),item);
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
-            document.querySelectorAll(SELECTORS.SEARCHFILTER_AUTHOR+ignoreSelector).forEach(function(item) {
+            document.querySelectorAll(SELECTORS.SEARCHFILTER_AUTHOR+coyoClickTracking.ignoreSelector).forEach(function(item) {
                 coyoClickTracking.filterClickTracking(coyoTrackingUtils.typeNameOverrides('Search-Author'),item);
-                item.setAttribute(ignore, "true");
+                item.setAttribute(coyoClickTracking.ignore, "true");
             });
         }
         if(TRACKINGSETTINGS.TRACKEXTERNALFILES) {
-            document.querySelectorAll(FILELINKSELECTOR).forEach(function(item) {
-                item.setAttribute(ignore+'-extfile', "true");
+            document.querySelectorAll(coyoClickTracking.FILELINKSELECTOR).forEach(function(item) {
+                item.setAttribute(coyoClickTracking.ignore+'-extfile', "true");
                 item.addEventListener('click', function(e) {
                     var title = coyoTrackingUtils.shortenString(item.textContent,200)
                     sendTrackingEvent('Media', coyoTrackingUtils.typeNameOverrides('View'), title, null);
@@ -149,6 +150,8 @@ var coyoClickTracking = {
         }
     }
 }
+
+coyoClickTracking.init();
 
 // page view tracking
 window.document.addEventListener('stateChangeSuccess', debounce(function() {
