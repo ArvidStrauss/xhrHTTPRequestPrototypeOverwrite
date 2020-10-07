e0637b7d26792cc7dafe7d431a94f85f1e6d6a4e
Marcus Biel <marcus.biel@t-systems.com>
2020-10-07 10:27:06 +0200

    LW: version 1.0.1

--- LW/gulpfile.js
+++ LW/gulpfile.js
@@ -1,8 +1,8 @@
 // projectdata is required (of course!)
 var projectData = {
     projectName: 'LW-Leoworld',
-    projectVersion: '1.0.0',
-    projectDate: '2020-08-07'
+    projectVersion: '1.0.1',
+    projectDate: '2020-10-07'
 }
 // override builds as array of {env,htmlFile} if needed, default is dev/test/prod setup (see common/gulpfile)
 // var builds = [
